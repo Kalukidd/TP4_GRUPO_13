4b3942c271a1d9fc155485b7dfe20c9a8e42af86
Gerson Nehemías Gutiérrez <101604791+Gerson-Gutierrez@users.noreply.github.com>
2025-04-27 19:22:35 -0300

    probe metodos
    estuve probando nuevos metodos

--- TP4_Grupo_13/Ejercicio3.aspx.cs
+++ TP4_Grupo_13/Ejercicio3.aspx.cs
@@ -11,27 +11,37 @@ namespace TP4_Grupo_13
 
     public partial class Ejercicio3 : System.Web.UI.Page
     {
-        //private const string cadenaConexion = "Data Source=GERSONGUTIERREZ\\SQLEXPRESS;Initial Catalog=Libreria;Integrated Security=True;Encrypt=False"
-        private const string cadenaConexion = "Data Source=DESKTOP-A61I0IB\\SQLEXPRESS;Initial Catalog=Libreria;Integrated Security=True;Encrypt=False";
+        private const string cadenaConexion = "Data Source=GERSONGUTIERREZ\\SQLEXPRESS;Initial Catalog=Libreria;Integrated Security=True;Encrypt=False";
+       // private const string cadenaConexion = "Data Source=DESKTOP-A61I0IB\\SQLEXPRESS;Initial Catalog=Libreria;Integrated Security=True;Encrypt=False";
         string consultaSQL = "SELECT * FROM Temas";
         protected void Page_Load(object sender, EventArgs e)
         {
+    
+
             if (!IsPostBack)
             {
                 using (SqlConnection connection = new SqlConnection(cadenaConexion))
                 {
                     connection.Open();
 
-                    SqlCommand cmd = new SqlCommand(consultaSQL, connection);
-                    SqlDataReader dr = cmd.ExecuteReader();
-                    ddlTemas.DataSource = dr;
+                    SqlDataAdapter da = new SqlDataAdapter(consultaSQL, connection);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    ddlTemas.DataSource = dt;
                     ddlTemas.DataTextField = "Tema";
                     ddlTemas.DataValueField = "IdTema";
                     ddlTemas.DataBind();
+
                     connection.Close();
                 }
             }
         }
+
+
+
+
+       
 
         protected void lbLibros_Click(object sender, EventArgs e)
         {
