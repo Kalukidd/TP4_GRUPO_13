a7c492d03f1ddf7a67352332f89c5ff74e490cdd
Marina Esttamatti <marinaesttamatti@gmail.com>
2025-04-27 23:59:46 -0300

    Correccion Validacion EJ2

--- TP4_Grupo_13/Ejercicio2.aspx
+++ TP4_Grupo_13/Ejercicio2.aspx
@@ -27,6 +27,29 @@
         font-size: small;
         display: block;
     }
+    .auto-style23 {
+        width: 35px;
+        height: 29px;
+    }
+    .auto-style24 {
+        width: 125px;
+        height: 29px;
+    }
+    .auto-style25 {
+        width: 30px;
+        height: 29px;
+    }
+    .auto-style26 {
+        width: 127px;
+        height: 29px;
+    }
+    .auto-style27 {
+        width: 268px;
+        height: 29px;
+    }
+    .auto-style28 {
+        height: 29px;
+    }
 </style>
 </head>
 <body>
@@ -42,35 +65,21 @@
                     <td>&nbsp;</td>
                 </tr>
                 <tr>
-                    <td class="auto-style6">&nbsp;</td>
-                    <td class="auto-style10">Id Producto:</td>
-                    <td class="auto-style5">&nbsp;</td>
-                    <td class="auto-style16">
+                    <td class="auto-style23"></td>
+                    <td class="auto-style24">Id Producto:</td>
+                    <td class="auto-style25"></td>
+                    <td class="auto-style26">
                         <asp:DropDownList ID="ddlIdProducto" runat="server">
                             <asp:ListItem Value="1">Igual a:</asp:ListItem>
                             <asp:ListItem Value="2">Mayor a:</asp:ListItem>
                             <asp:ListItem Value="3">Menor a:</asp:ListItem>
                         </asp:DropDownList>
                     </td>
-                    <td class="auto-style3">
+                    <td class="auto-style27">
                         <asp:TextBox ID="txtProducto" runat="server" Width="245px"></asp:TextBox>
                     </td>
-                    <td>
-                        <asp:RegularExpressionValidator ID="revProducto" runat="server"
-                            ControlToValidate="txtProducto"
-                            ValidationExpression="^[1-9]\d*$"
-                            ErrorMessage="* Solo números enteros positivos, sin espacios ni letras"
-                            CssClass="validator"
-                            Display="Dynamic" />
-                        <asp:RequiredFieldValidator ID="rfvProducto" runat="server" ControlToValidate="txtProducto" Display="Dynamic" ErrorMessage="RequiredFieldValidator" ForeColor="Red">Debe ingresar al menos un número</asp:RequiredFieldValidator>
-                        <asp:RangeValidator ID="rvProducto" runat="server"
-                            ControlToValidate="txtProducto"
-                            MinimumValue="1"
-                            MaximumValue="999999"
-                            Type="Integer"
-                            ErrorMessage="* El número debe ser mayor o igual a 1"
-                            CssClass="validator"
-                            Display="Dynamic" />
+                    <td class="auto-style28">
+                        <asp:Label ID="lblProdER" runat="server" ForeColor="Red"></asp:Label>
                     </td>
                 </tr>
                 <tr>
@@ -88,21 +97,7 @@
                         <asp:TextBox ID="txtCategoria" runat="server" Width="243px"></asp:TextBox>
                     </td>
                     <td class="auto-style22">
-                        <asp:RegularExpressionValidator ID="revCategoria" runat="server"
-                            ControlToValidate="txtCategoria"
-                            ValidationExpression="^[1-9]\d*$"
-                            ErrorMessage="* Solo números enteros positivos, sin espacios ni letras"
-                            CssClass="validator"
-                            Display="Dynamic" />
-                        <asp:RequiredFieldValidator ID="rfvCategoria" runat="server" ControlToValidate="txtCategoria" Display="Dynamic" ErrorMessage="RequiredFieldValidator" ForeColor="Red">Debe ingresar al menos un número</asp:RequiredFieldValidator>
-                        <asp:RangeValidator ID="rvCategoria" runat="server"
-                            ControlToValidate="txtCategoria"
-                            MinimumValue="1"
-                            MaximumValue="999999"
-                            Type="Integer"
-                            ErrorMessage="* El número debe ser mayor o igual a 1"
-                            CssClass="validator"
-                            Display="Dynamic" />
+                        <asp:Label ID="lblCatER" runat="server" ForeColor="Red"></asp:Label>
                     </td>
                 </tr>
                 <tr>
--- TP4_Grupo_13/Ejercicio2.aspx.cs
+++ TP4_Grupo_13/Ejercicio2.aspx.cs
@@ -6,6 +6,7 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data.SqlClient;
 using System.Data;
+using System.Text.RegularExpressions;
 
 
 
@@ -16,8 +17,8 @@ namespace TP4_Grupo_13
     public partial class Ejercicio2 : System.Web.UI.Page
 
     {
-        private const string cadenaConexion = @"Data Source=DESKTOP-IN37CD7\SQLEXPRESS;Initial Catalog=Neptuno;Integrated Security=True;TrustServerCertificate=True";
-        // private const string cadenaConexion = @"Data Source=LENOVO\SQLEXPRESS;Initial Catalog=Neptuno;Integrated Security=True;Encrypt=False";
+        //private const string cadenaConexion = @"Data Source=DESKTOP-IN37CD7\SQLEXPRESS;Initial Catalog=Neptuno;Integrated Security=True;TrustServerCertificate=True";
+        private const string cadenaConexion = @"Data Source=LENOVO\SQLEXPRESS;Initial Catalog=Neptuno;Integrated Security=True;Encrypt=False";
         // private const string cadenaConexion = "Data Source=GERSONGUTIERREZ\\SQLEXPRESS;Initial Catalog=Neptuno;Integrated Security=True;Encrypt=False";
         // private const string cadenaConexion = "Data Source=DESKTOP-A61I0IB\\SQLEXPRESS;Initial Catalog=Neptuno;Integrated Security=True;Encrypt=False";
         //private const string cadenaConexion = "Data Source=KALU\\SQLEXPRESS;Initial Catalog=Neptuno;Integrated Security=True;Encrypt=False";
@@ -56,18 +57,36 @@ namespace TP4_Grupo_13
 
         protected void btnFiltrar_Click(object sender, EventArgs e)
         {
-
             string idProducto = txtProducto.Text;
             string filtrodeProducto = ddlIdProducto.SelectedValue;
             string idCategoria = txtCategoria.Text;
             string filtrodeCategoria = ddlIdCategoria.SelectedValue;
 
-           
+            string regex = @"^[0-9,$]*$";
+            bool esValidoPROD = Regex.IsMatch(idProducto, regex);
+            bool esValidoCAT = Regex.IsMatch(idCategoria, regex);
+
+            if (string.IsNullOrEmpty(idProducto) && string.IsNullOrEmpty(idCategoria)) {
+                lblProdER.Text = "Debe ingresar al menos un número";
+                lblCatER.Text = "Debe ingresar al menos un número";
+            }
+
+            if (!string.IsNullOrEmpty(idProducto) && !esValidoPROD) {
+                lblProdER.Text = "* El número debe ser mayor o igual a 1";
+            }
+
+            if (!string.IsNullOrEmpty(idCategoria) && !esValidoCAT)
+            {
+                lblCatER.Text = "* El número debe ser mayor o igual a 1";
+            }
+            
             string consulta = "SELECT IdProducto, NombreProducto, IdCategoría, CantidadPorUnidad, PrecioUnidad FROM Productos WHERE 1=1";
 
            
-            if (!string.IsNullOrEmpty(idProducto))
+            if (!string.IsNullOrEmpty(idProducto) && esValidoPROD)
             {
+                lblProdER.Text = "";
+                lblCatER.Text = "";
                 switch (filtrodeProducto)
                 {
                     case "1":  // Igual a
@@ -82,8 +101,10 @@ namespace TP4_Grupo_13
                 }
             }
 
-            if (!string.IsNullOrEmpty(idCategoria))
+            if (!string.IsNullOrEmpty(idCategoria) && esValidoCAT)
             {
+                lblProdER.Text = "";
+                lblCatER.Text = "";
                 if (!consulta.Contains("WHERE"))
                 {
                     consulta += " WHERE 1=1";
@@ -142,44 +163,51 @@ namespace TP4_Grupo_13
                          break;
                  }*/
 
-           /* using (SqlConnection connection = new SqlConnection(cadenaConexion))
-            {
-                connection.Open();
-                SqlCommand cmd = new SqlCommand(consulta, connection);
-                cmd.Parameters.AddWithValue("@IdProducto", txtProducto.Text);
-                SqlDataReader reader = cmd.ExecuteReader();
-                gvProductos.DataSource = reader;
-                gvProductos.DataBind();
-            }*/
-
-            using (SqlConnection cn = new SqlConnection(cadenaConexion))
-            {
-                using (SqlDataAdapter cmd = new SqlDataAdapter(consulta, cn))
+            /* using (SqlConnection connection = new SqlConnection(cadenaConexion))
+             {
+                 connection.Open();
+                 SqlCommand cmd = new SqlCommand(consulta, connection);
+                 cmd.Parameters.AddWithValue("@IdProducto", txtProducto.Text);
+                 SqlDataReader reader = cmd.ExecuteReader();
+                 gvProductos.DataSource = reader;
+                 gvProductos.DataBind();
+             }*/
+            if (esValidoCAT || esValidoPROD) {
+                using (SqlConnection cn = new SqlConnection(cadenaConexion))
                 {
-                    if (!string.IsNullOrEmpty(idProducto))
+                    using (SqlDataAdapter cmd = new SqlDataAdapter(consulta, cn))
                     {
-                        cmd.SelectCommand.Parameters.AddWithValue("@IdProducto", idProducto);
-                    }
-                    if (!string.IsNullOrEmpty(idCategoria))
-                    {
-                        cmd.SelectCommand.Parameters.AddWithValue("@IdCategoría", idCategoria);
-                    }
+                        if (!string.IsNullOrEmpty(idProducto))
+                        {
+                            cmd.SelectCommand.Parameters.AddWithValue("@IdProducto", idProducto);
+                        }
+                        if (!string.IsNullOrEmpty(idCategoria))
+                        {
+                            cmd.SelectCommand.Parameters.AddWithValue("@IdCategoría", idCategoria);
+                        }
 
-                    DataSet ds = new DataSet();
-                    cmd.Fill(ds, "Productos");
-                    gvProductos.DataSource = ds;
-                    gvProductos.DataBind();
+                        DataSet ds = new DataSet();
+                        cmd.Fill(ds, "Productos");
+                        gvProductos.DataSource = ds;
+                        gvProductos.DataBind();
+                    }
                 }
+                // Limpia los TextBox después de filtrar
+                txtProducto.Text = "";
+                txtCategoria.Text = "";
             }
-            // Limpia los TextBox después de filtrar
-            txtProducto.Text = "";
-            txtCategoria.Text = "";
+            
+            
         }
 
         protected void btnQuitarFiltro_Click(object sender, EventArgs e)
         {
             txtCategoria.Text = "";
             txtProducto.Text = "";
+
+            lblProdER.Text = "";
+            lblCatER.Text = "";
+
             using (SqlConnection connection = new SqlConnection(cadenaConexion))
             {
                 connection.Open();
